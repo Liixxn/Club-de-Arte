c4b5cea18dd1a0b19fc9f91c84429f7430a15626
miguelwang125 <60920243+miguelwang125@users.noreply.github.com>
2022-12-27 12:26:54 +0100

    Actualizacion

--- ClubArte/js/temasInteres.js
+++ ClubArte/js/temasInteres.js
@@ -1,24 +1,24 @@
-let myImage = document.getElementById("mainImage");
-
-let imageArray = ["imgs/portada_1.jpg", "imgs/1586283508_022049_1586284162_noticia_normal_recorte1.jpg", "imgs/16474297191037.jpg"];
 let imageIndex = 0;
 
+let imageArray = ["imgs/portada_1.jpg", "imgs/1586283508_022049_1586284162_noticia_normal_recorte1.jpg", "imgs/16474297191037.jpg"];
+
 function changeImage() {
-    myImage.setAttribute("src", imageArray[imageIndex]);
+
+    $('#banner').css("background-image", 'url(' + imageArray[imageIndex] + ')');
+
+    //myImage.setAttribute("src", imageArray[imageIndex]);
     imageIndex++;
     if (imageIndex >= imageArray.length){
         imageIndex = 0;
     }
 }
 
-var intervalHandle = setInterval(changeImage, 1000);
+$(function() {
+    
+    setInterval(changeImage, 2000);
+    
+});
 
 //myImage.onclick = function() {
 //    clearInterval(intervalHandle);
 //};
-
-var imagen = new Array["imgs/635dbec96075400eb6c18599a9be2ad0.jpg", "imgs/Taller_Infantil.jpg", "imgs/taller-pintura-06-1024x675.jpg"];
-
-function nextImage() {
-    document.getElementById("actividades").src
-}
