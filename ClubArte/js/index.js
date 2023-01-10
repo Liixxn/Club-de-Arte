cd4ae05f3e6bcf7d149f02f57cb64a4b1536387a
Lian Salmerón López <lembajosa@gmail.com>
2023-01-10 18:45:31 +0100

    Añadido de submenu en el usuario y un form en el footer

--- ClubArte/js/index.js
+++ ClubArte/js/index.js
@@ -1,3 +1,17 @@
 
 
+let tituloEmail = document.getElementById("texto-titulo-email");
+let nombreEmail = document.getElementById("texto-nombre-email");
+let mensaje = document.getElementById("contenido-mensaje");
+
+function validarEmail() {
+
+    if (nombreEmail.value == "") {
+        console.log("El campo nombre no puede estar vacío");
+        return false;
+    }
+
+    return true
+
+}
 
